96bc0aedaf93fd92bc4ec7a4e58feea0943a1a89
Paul H Cassidy <qawarrior@gmail.com>
2018-01-01 12:24:25 -0800

    Fine tuning Get-RlsrLogEntry function preparing to integrate logging into the Rlsr Engine Tasks

--- cmds/New-RlsrLogEntry.ps1
+++ cmds/New-RlsrLogEntry.ps1
@@ -3,27 +3,28 @@
 function New-RlsrLogEntry {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory, HelpMessage = 'Time Log Entry was created')]
+        [Parameter(HelpMessage = 'Time Log Entry was created')]
         [string]
-        $Timestamp = [DateTime]::NoW.ToString('yyyyMMddHHmmss'),
+        $Timestamp = [DateTime]::NoW.ToString('s'),
 
-        [Parameter(Mandatory, HelpMessage = 'Controls what types of entries are logged')]
+        [Parameter(Mandatory, HelpMessage = 'The log entries serverity level')]
         [ValidateSet('INFO', 'WARN', 'ERROR', 'FATAL', 'DEBUG', 'VERBOSE')]
         [string]
         $Level,
 
-        [Parameter(Mandatory, HelpMessage = 'Context named block that generated the LogEntry')]
+        [Parameter(Mandatory, HelpMessage = 'Unique Task Run Name Id. derived from TaskName and the time the task started running')]
         [ValidateNotNullOrEmpty()]
         [string]
         $RunName,
 
-        [Parameter(Mandatory, HelpMessage = 'Context named block that generated the LogEntry')]
+        [Parameter(Mandatory, HelpMessage = 'Context is the name (Id) of the script, block, function, or variable generating the entry')]
         [ValidateNotNullOrEmpty()]
         [string]
         $Context,
 
-        [Parameter(Mandatory, HelpMessage = 'Controls what types of entries are logged')]
+        [Parameter(Mandatory, HelpMessage = 'Description of a specific tasks event')]
         [ValidateNotNullOrEmpty()]
+        [string]
         $Message
     )
     
