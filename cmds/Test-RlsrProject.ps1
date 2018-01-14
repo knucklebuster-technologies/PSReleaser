89c4040652c95dcfe1a9e3ae79a41c6fcee822a4
Paul H Cassidy <qawarrior@gmail.com>
2018-01-14 09:28:18 -0800

    added renamed files

---
+++ cmds/Test-RlsrProject.ps1
@@ -0,0 +1,16 @@
+
+
+function Test-RlsrProject {
+    [CmdletBinding()]
+    param (
+       [string]$Path,
+       [string]$Name 
+    )
+    
+    end {
+        # TODO: Verify that rlsrcr1.psd1 project file exists
+        # TODO: Create Lock File rlsrcr1.lock.json if not present in project folder
+        # TODO: Return true if project file exists and lock file is present or created
+        # TODO: Return false if project file does not exists or lock file fails to be created
+    }
+}
